b26fca4f3ea4c6c1f9b2d0830d795d1aa396cdea
John Doeirma <irmaaa96@hotmail.com>
2021-05-09 21:54:40 +0200

    stared with the game site

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import WelcomeSite from "./components/WelcomeSite";
+import SnakeGame from "./components/SnakeGame";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={WelcomeSite} />
+        <Route path="/game" component={SnakeGame} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/components/SnakeGame.js
@@ -0,0 +1,190 @@
+import React, { useCallback, useEffect, useState } from "react";
+import Snake from "./Snake";
+
+const getRandomCoordinates = () => {
+  let min = 1;
+  let max = 90;
+  let x = Math.floor((Math.random() * (max - min + 1) + min) / 2) * 2;
+  let y = Math.floor((Math.random() * (max - min + 1) + min) / 2) * 2;
+  return [x, y];
+};
+
+const SnakeGame = () => {
+  const [speed, setSpeed] = useState(200);
+  const [direction, setDirection] = useState("RIGHT");
+  const [snakeDots, setSnakeDots] = useState([
+    [0, 0],
+    [2, 0]
+  ]);
+  const [pause, setPause] = useState(true);
+  const [time, setTime] = useState({ ms: 0, s: 0, m: 0, h: 0 });
+  const [interv, setInterv] = useState();
+  const [status, setStatus] = useState(0);
+
+  useEffect(() => {
+    if (pause) return;
+    checkIfOutOfBorders();
+    checkIfCollapsed();
+    setTimeout(() => moveSnake(snakeDots, checkIfEat()), speed);
+  }, [snakeDots, pause]);
+
+  useEffect(() => {
+    // document.onkeydown = onKeyDown;
+    const onKeyDown = (e) => {
+      e = e || window.event;
+      switch (e.keyCode) {
+        case 38:
+          console.log("direction", direction);
+          !["DOWN", "UP"].includes(direction) && setDirection("UP");
+          break;
+        case 40:
+          !["DOWN", "UP"].includes(direction) && setDirection("DOWN");
+          break;
+        case 37:
+          !["LEFT", "RIGHT"].includes(direction) && setDirection("LEFT");
+          break;
+        case 39:
+          !["LEFT", "RIGHT"].includes(direction) && setDirection("RIGHT");
+          break;
+
+        default:
+          break;
+      }
+    };
+
+    document.addEventListener("keydown", onKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", onKeyDown);
+      console.log("direction return", direction); // useEffect precedente
+    };
+  }, [direction, setDirection]);
+
+  const moveSnake = useCallback(
+    (snakeDots, eaten) => {
+      let dots = [...snakeDots];
+      let head = dots[dots.length - 1];
+
+      switch (direction) {
+        case "RIGHT":
+          head = [head[0] + 2, head[1]];
+          break;
+        case "LEFT":
+          head = [head[0] - 2, head[1]];
+          break;
+        case "DOWN":
+          head = [head[0], head[1] + 2];
+          break;
+        case "UP":
+          head = [head[0], head[1] - 2];
+          break;
+
+        default:
+          break;
+      }
+      if (direction) {
+        dots.push(head);
+
+        setSnakeDots([...dots]);
+      }
+    },
+    [direction]
+  );
+
+  const checkIfOutOfBorders = () => {
+    let head = snakeDots[snakeDots.length - 1];
+    if (head[0] >= 100 || head[1] >= 100 || head[0] < 0 || head[1] < 0) {
+      onGameOver();
+    }
+  };
+
+  const checkIfCollapsed = () => {
+    let snake = [...snakeDots];
+    let head = snake[snake.length - 1];
+    snake.pop();
+    snake.forEach((dot) => {
+      if (head[0] === dot[0] && head[1] === dot[1]) {
+        onGameOver();
+      }
+    });
+  };
+
+  const checkIfEat = () => {
+    let head = snakeDots[snakeDots.length - 1];
+  };
+
+  const onGameOver = () => {
+    setSnakeDots([
+      [0, 0],
+      [2, 0]
+    ]);
+    setDirection(null);
+    window.location.reload();
+  };
+
+  const start = () => {
+    run();
+    setStatus(1);
+    setInterv(setInterval(run, 10));
+  };
+
+  let updatedMs = time.ms,
+    updatedS = time.s,
+    updatedM = time.m,
+    updatedH = time.h;
+
+  const run = () => {
+    if (updatedM === 60) {
+      updatedH++;
+      updatedM = 0;
+    }
+
+    if (updatedS === 60) {
+      updatedM++;
+      updatedS = 0;
+    }
+
+    if (updatedMs === 100) {
+      updatedS++;
+      updatedMs = 0;
+    }
+    updatedMs++;
+    return setTime({ ms: updatedMs, s: updatedS, m: updatedM, h: updatedH });
+  };
+
+  const stop = () => {
+    clearInterval(interv);
+    setStatus(2);
+  };
+
+  return (
+    <>
+      <div className="score">
+        {" "}
+        <h3>Score: </h3>
+        <span className="score-span"> {snakeDots.length - 2}</span>
+      </div>
+
+      <div className="game-area">
+        <Snake snakeDots={snakeDots} />
+      </div>
+
+      <div className="btn">
+        <button
+          onClick={() => {
+            setPause((p) => !p);
+            if (pause) {
+              start();
+            } else {
+              stop();
+            }
+          }}
+        >
+          {pause ? "PLAY" : "PAUSE"}
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default SnakeGame;
